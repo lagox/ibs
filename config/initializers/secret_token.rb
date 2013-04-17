5ae7967eac79dc049e314d8482db6a7e09d8f689
Yerlan Kussainov <lagox.mail@gmail.com>
2013-04-17 12:53:38 +0600

    .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogIbecs::Application.config.secret_token = '07bd439a86cd13e0ff22e33b1ffb71d1a50d0778693a3f91b08ad86f3baa849b267d215b60b55a28ae3db954d26286b1cacd3f50889d271f3c686c9bbaf719ba'
